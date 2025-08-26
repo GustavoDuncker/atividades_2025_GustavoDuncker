abc2a9c78be8b1fb450202f7334f68037b0e302d
GustavoDuncker <gustavo_duncker@estudante.sesisenai.org.br>
2025-08-26 08:21:19 -0300

    5

--- Biblioteca/dbG.php
+++ Biblioteca/dbG.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "bibliotecaDuncker");
+$conn = new mysqli("localhost", "root", "root", "bibliotecaDuncker");
 
 if ($conn->connect_error) {
     die("Erro na conexão");
